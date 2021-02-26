3d92ab89f0af21a3d4a45537725426c13b8744c0
Moberd <60620556+Moberd@users.noreply.github.com>
2021-02-26 18:24:28 +0300

    login activity

--- app/src/main/java/com/smri/smarttracker/fragments/profile/ProfileFragment.java
+++ app/src/main/java/com/smri/smarttracker/fragments/profile/ProfileFragment.java
@@ -1,15 +1,18 @@
 package com.smri.smarttracker.fragments.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.smri.smarttracker.R;
+import com.smri.smarttracker.activities.ChemEditor;
 import com.smri.smarttracker.utils.FabFragmentListener;
 
 public class ProfileFragment extends Fragment {
@@ -17,10 +20,25 @@ public class ProfileFragment extends Fragment {
     public FabFragmentListener listener;
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, null);
+        View view = inflater.inflate(R.layout.fragment_profile, null);
+        Button signBtn = view.findViewById(R.id.singBtn);
+        signBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        }
+        );
+        return view;
     }
 
     public void editProfile(){
         Toast.makeText(getContext(), "PROFILE CLICKED", Toast.LENGTH_SHORT).show();
+    }
+
+    public void signIn(){
+        Toast.makeText(getContext(), "REG SCREEN CLICKED", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(getContext(), SignInOut.class);
+        getContext().startActivity(intent);
     }
 }
--- app/src/main/java/com/smri/smarttracker/fragments/profile/SignInOut.java
+++ app/src/main/java/com/smri/smarttracker/fragments/profile/SignInOut.java
@@ -1,22 +1,21 @@
-package com.smri.smarttracker.activities;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.smri.smarttracker.fragments.profile;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.smri.smarttracker.R;
 
-public class ChemEditor extends AppCompatActivity {
+public class SignInOut extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chem_editor);
-
-        ImageButton backBtn = findViewById(R.id.backBtn);
+        setContentView(R.layout.reg_layout);
+        ImageButton backBtn = findViewById(R.id.btnBack);
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
