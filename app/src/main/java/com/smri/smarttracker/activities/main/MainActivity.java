3c666eab7c6f4af016372077041484f335154a8f
dlebedyuk <daslebedyuk@gmail.com>
2021-03-19 13:38:21 +0300

    splash

--- app/src/main/java/com/smri/smarttracker/activities/main/MainActivity.java
+++ app/src/main/java/com/smri/smarttracker/activities/main/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         loadFragment(currFragment);
