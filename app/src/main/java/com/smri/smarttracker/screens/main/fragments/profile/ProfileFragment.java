ec25a458a0a85e59b174c090e2f4e1ac625ac936
Moberd <ocheretov.ilya@yandex.ru>
2021-05-21 19:38:59 +0300

    login screen change

--- app/src/main/java/com/smri/smarttracker/screens/main/fragments/profile/ProfileFragment.java
+++ app/src/main/java/com/smri/smarttracker/screens/main/fragments/profile/ProfileFragment.java
@@ -1,12 +1,18 @@
 package com.smri.smarttracker.screens.main.fragments.profile;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -15,6 +21,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.smri.smarttracker.R;
 import com.smri.smarttracker.screens.splash.SplashActivity;
 import com.smri.smarttracker.utils.FabFragmentListener;
+
+import es.dmoral.toasty.Toasty;
 
 public class ProfileFragment extends Fragment {
     static boolean editMode = false;
@@ -26,10 +34,13 @@ public class ProfileFragment extends Fragment {
     ImageView btnEditUserName;
     TextView tvEmail;
     TextView tvPhone;
+    TextView tvLabNumber;
+    TextView tvUserName;
     TextView tvLab;
     TextView btnSignOut;
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        editMode = false;
         View view = inflater.inflate(R.layout.fragment_profile, null);
         btnEditEmail = view.findViewById(R.id.edit_email);
         btnEditPhone = view.findViewById(R.id.edit_phone_number);
@@ -39,8 +50,10 @@ public class ProfileFragment extends Fragment {
         btnSignOut = view.findViewById(R.id.singBtn);
         tvEmail = view.findViewById(R.id.tv_email);
         tvPhone = view.findViewById(R.id.tv_phone_number);
-        tvLab = view.findViewById(R.id.tv_laboratory_number);
-
+        tvLabNumber = view.findViewById(R.id.tv_laboratory_number);
+        tvLab = view.findViewById(R.id.tv_laboratory);
+        tvUserName = view.findViewById(R.id.tv_user_name);
+        setUpListeners();
         btnSignOut.setOnClickListener(v -> exitFirebase());
         return view;
     }
@@ -61,18 +74,78 @@ public class ProfileFragment extends Fragment {
         editMode = true;
         btnEditEmail.setVisibility(View.VISIBLE);
         btnEditPhone.setVisibility(View.VISIBLE);
-        btnEditLabNumber.setVisibility(View.VISIBLE);
+        //btnEditLabNumber.setVisibility(View.VISIBLE);
         btnEditUserName.setVisibility(View.VISIBLE);
-        btnEditUserName.setVisibility(View.VISIBLE);
+        //btnEditLab.setVisibility(View.VISIBLE);
     }
 
     public void exitEditMode(){
         editMode = false;
         btnEditEmail.setVisibility(View.GONE);
         btnEditPhone.setVisibility(View.GONE);
-        btnEditLabNumber.setVisibility(View.GONE);
+        //btnEditLabNumber.setVisibility(View.GONE);
         btnEditUserName.setVisibility(View.GONE);
-        btnEditUserName.setVisibility(View.GONE);
+        //btnEditUserLab.setVisibility(View.GONE);
+    }
+
+    void setUpListeners(){
+        btnEditUserName.setOnClickListener(v -> {
+            Dialog dialog = new Dialog(getActivity());
+            dialog.setContentView(R.layout.edit_dialog_layout);
+            dialog.setTitle("Change Your Name");
+            Button btnSave = dialog.findViewById(R.id.btnSave);
+            EditText editText = dialog.findViewById(R.id.etNew);
+            btnSave.setOnClickListener(dialogV -> {
+                String newText = editText.getText().toString();
+                tvUserName.setText(newText);
+                Toasty.info(getContext(),newText).show();
+                dialog.dismiss();
+            });
+            Button btnCancel = dialog.findViewById(R.id.btnCancel);
+            btnCancel.setOnClickListener(dialogV -> {
+                dialog.dismiss();
+            });
+            dialog.show();
+        });
+
+        btnEditEmail.setOnClickListener(v -> {
+            Dialog dialog = new Dialog(getActivity());
+            dialog.setContentView(R.layout.edit_dialog_layout);
+            dialog.setTitle("Change Email");
+            Button btnSave = dialog.findViewById(R.id.btnSave);
+            EditText editText = dialog.findViewById(R.id.etNew);
+            btnSave.setOnClickListener(dialogV -> {
+                String newText = editText.getText().toString();
+                tvEmail.setText(newText);
+                Toasty.info(getContext(),newText).show();
+                dialog.dismiss();
+            });
+            Button btnCancel = dialog.findViewById(R.id.btnCancel);
+            btnCancel.setOnClickListener(dialogV -> {
+                dialog.dismiss();
+            });
+            dialog.create();
+            dialog.show();
+        });
+
+        btnEditPhone.setOnClickListener(v -> {
+            Dialog dialog = new Dialog(getActivity());
+            dialog.setContentView(R.layout.edit_dialog_layout);
+            dialog.setTitle("Change Phone Number");
+            Button btnSave = dialog.findViewById(R.id.btnSave);
+            EditText editText = dialog.findViewById(R.id.etNew);
+            btnSave.setOnClickListener(dialogV -> {
+                String newText = editText.getText().toString();
+                tvPhone.setText(newText);
+                Toasty.info(getContext(),newText).show();
+                dialog.dismiss();
+            });
+            Button btnCancel = dialog.findViewById(R.id.btnCancel);
+            btnCancel.setOnClickListener(dialogV -> {
+                dialog.dismiss();
+            });
+            dialog.show();
+        });
     }
 
     public void exitFirebase(){
